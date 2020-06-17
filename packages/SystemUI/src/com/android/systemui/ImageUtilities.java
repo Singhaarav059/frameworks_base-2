5ae02d953227c6b1a1f75567fbf580d9132d7f68
Singhaarav059 <singhaarav059@gmail.com>
2020-06-17 12:29:47 +0000

    Revert "SystemUI: Introduce Adjustable Blur Intensity [1/2]"
    This reverts commit dace253818dce9283b213b1c1211efc055e318e3.

--- packages/SystemUI/src/com/android/systemui/ImageUtilities.java
+++ packages/SystemUI/src/com/android/systemui/ImageUtilities.java
@@ -67,7 +67,7 @@ public class ImageUtilities {
     }
 
 /* blur routine */
-    public static Bitmap blurImage(Context context, Bitmap image, int intensity) {
+    public static Bitmap blurImage(Context context, Bitmap image) {
         float BITMAP_SCALE = 0.4f;
         float BLUR_RADIUS = 7.5f;
 
@@ -81,9 +81,6 @@ public class ImageUtilities {
         ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
         Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
         Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);  
-        if (intensity > 0 && intensity <= 100) {
-            BLUR_RADIUS = (float) intensity * 0.25f;
-        }
         theIntrinsic.setRadius(BLUR_RADIUS);
         theIntrinsic.setInput(tmpIn);
         theIntrinsic.forEach(tmpOut);
