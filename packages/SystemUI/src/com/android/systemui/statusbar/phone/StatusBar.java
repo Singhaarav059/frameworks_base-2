d8501b9898a814ea92171e46b7131a2879d6ff6b
Singhaarav059 <singhaarav059@gmail.com>
2020-06-17 12:36:10 +0000

    Revert "Make QS blur togglable [1/2]"
    This reverts commit 1312941ee10f48135f27253a035872583539d3b2.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1288,7 +1288,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         int QSBlurAlpha = Math.round(255.0f * mNotificationPanel.getExpandedFraction());
 
-        if (QSBlurAlpha > 0 && !blurperformed && !mIsKeyguard && isQSBlurEnabled()) {
+        if (QSBlurAlpha > 0 && !blurperformed && mState != StatusBarState.KEYGUARD) {
             Bitmap bittemp = ImageUtilities.blurImage(mContext, ImageUtilities.screenshotSurface(mContext));
             Drawable blurbackground = new BitmapDrawable(mContext.getResources(), bittemp);
             blurperformed = true;
@@ -1341,11 +1341,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 }
             });
         }
-    }
-
-    private boolean isQSBlurEnabled() {
-        return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QS_BLUR, 1) != 0;
     }
 
     private void brightnessControl(MotionEvent event) {
